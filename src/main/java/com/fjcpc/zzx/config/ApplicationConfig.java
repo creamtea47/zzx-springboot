d4d11b7bdd531d4a3f9682fc0c3baed528bf276e
xiaolu <nellpoi@icloud.com>
2022-06-20 14:14:52 +0800

    - 更新版本号为1.0.7 - 新增jackson 中对null的处理

--- src/main/java/com/fjcpc/zzx/config/ApplicationConfig.java
+++ src/main/java/com/fjcpc/zzx/config/ApplicationConfig.java
@@ -5,5 +5,5 @@ package com.fjcpc.zzx.config;
  * @create 2022-06-17 10:50
  **/
 public class ApplicationConfig {
-    public static final String VERSION = "1.0.6";
+    public static final String VERSION = "1.0.7";
 }
