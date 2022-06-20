002009e4232c36ea0ccf8ead1b2043b312828085
xiaolu <nellpoi@icloud.com>
2022-06-20 15:11:17 +0800

    - 更新版本号为1.0.9 - 修正UpdateInfo接口无法更新用户实际名字的问题

--- src/main/java/com/fjcpc/zzx/config/ApplicationConfig.java
+++ src/main/java/com/fjcpc/zzx/config/ApplicationConfig.java
@@ -5,5 +5,5 @@ package com.fjcpc.zzx.config;
  * @create 2022-06-17 10:50
  **/
 public class ApplicationConfig {
-    public static final String VERSION = "1.0.8";
+    public static final String VERSION = "1.0.9";
 }
